14d15e5861d5d087728b62db92e6e5c549200571
Sergio Banegas <sbanegas92@gmail.com>
2017-10-20 12:55:58 +0200

    Config URIS structures improved

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-export const API_URL = "http://localhost:8000/auth";
+export const BACKEND_URL = "http://localhost:8000";
+const URI_AUTH = "/auth";
+const URI_API =  "/api";
 
-export const URI_LOGIN = "/login";
-export const URI_SIGNUP = "/signup";
+export const URI_LOGIN = URI_AUTH + "/login";
+export const URI_SIGNUP = URI_AUTH + "/signup";
--- src/services/http.js
+++ src/services/http.js
@@ -1,10 +1,10 @@
-import { API_URL} from '../config';
+import { BACKEND_URL} from '../config';
 
 var axios = require('axios');
 
 export const post = function(endpoint, body, params){
   return new Promise((resolve, reject) => {
-    axios.post(API_URL + endpoint, body).then(response => {
+    axios.post(BACKEND_URL + endpoint, body).then(response => {
       if (!response.data){
         reject();
       } else {
